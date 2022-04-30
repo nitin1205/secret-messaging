aff2d68521e2e4b4834e7a3ec1d5224aa48a585a
ShadyCoder777 <99088343+ShadyCoder777@users.noreply.github.com>
2022-04-30 18:21:59 +0530

    made basic strcuture

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkinput = document.querySelector('#link-input');
+    linkinput.value = `${window.location}#${encrypted}`;
+    linkinput.select();
+});
+
